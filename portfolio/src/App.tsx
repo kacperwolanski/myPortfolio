e1aa8dd69327aad0d34b84bf9c7673da7d143f12
kacper-codespot <kwolanski@codespot.io>
2024-08-01 17:27:55 +0200

    add mui

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,5 +1,27 @@
-function App() {
-  return <div>Wercia is very sweet</div>;
-}
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+  background-color: #f8f9fa;
+`;
+
+const Title = styled.h1`
+  color: #343a40;
+  font-weight: 400;
+`;
+
+const App: React.FC = () => {
+  return (
+    <Container>
+      <Title>Kacper Wolański</Title>
+    </Container>
+  );
+};
 
 export default App;
