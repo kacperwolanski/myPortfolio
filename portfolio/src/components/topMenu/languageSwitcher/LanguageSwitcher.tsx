595a86cfe4bbd30aa18bda887f537b07db48930f
kacper-codespot <kwolanski@codespot.io>
2024-09-28 23:53:51 +0200

    change flags

--- portfolio/src/components/topMenu/languageSwitcher/LanguageSwitcher.tsx
+++ portfolio/src/components/topMenu/languageSwitcher/LanguageSwitcher.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { FlagButton } from "./languageSwitcher.styles";
-import { British, Poland } from "shared/assets/icons/Icons";
 
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
@@ -15,11 +14,19 @@ const LanguageSwitcher = () => {
     <>
       {currentLenIsPolish ? (
         <FlagButton onClick={() => handleChangeLanguage("en")}>
-          <Poland />
+          <img
+            style={{ width: "50px", height: "30px" }}
+            alt="PL"
+            src="https://github.com/user-attachments/assets/1ec77f8b-7048-4571-a716-7182562ba576"
+          />
         </FlagButton>
       ) : (
         <FlagButton onClick={() => handleChangeLanguage("pl")}>
-          <British />
+          <img
+            style={{ width: "50px", height: "30px" }}
+            alt="ENG"
+            src="https://github.com/user-attachments/assets/7a429467-527b-42a0-8c92-d84ad6ca91a0"
+          />
         </FlagButton>
       )}
     </>
